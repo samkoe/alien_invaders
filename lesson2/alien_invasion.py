f9ab4ec72046be334317ec8ffcb844d61cfbb0d6
PiMaker <goodbirding@gmail.com>
2017-02-20 15:33:41 -0700

    Added Lesson 2
    In Lesson 2 we add left and right movement to the ship, keep it from going off the screen (boundary checks), and adjust the speed factor of its movement.

---
+++ lesson2/alien_invasion.py
@@ -0,0 +1,31 @@
+# alien_invasion.py
+
+import pygame
+
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+
+def run_game():
+	# Initialize game and create a screen object
+	pygame.init()
+	ai_settings = Settings()
+	screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+	pygame.display.set_caption("Alien Invasion")
+
+	# Set the background color
+	bg_color = (230, 230, 230)
+
+	# Make a ship
+	ship = Ship(ai_settings, screen)
+
+	# Start the main loop for the game
+	while True:
+
+		gf.check_events(ship)	# Watch for keyboard and mouse events
+		ship.update()
+		gf.update_screen(ai_settings, screen, ship)	# Update images on screen
+
+		
+
+run_game()
---
+++ lesson2/settings.py
@@ -0,0 +1,14 @@
+# settings.py
+
+class Settings():
+	"""A class to store all settings for Alien Invasion."""
+
+	def __init__(self):
+		"""Initialize the game's settings."""
+		# Screen settings
+		self.screen_width = 600
+		self.screen_height = 500
+		self.bg_color = (230, 230, 230)
+
+		# Ship settings
+		self.ship_speed_factor = 1.5
